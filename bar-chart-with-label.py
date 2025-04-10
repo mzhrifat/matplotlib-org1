fdd18a8b143b18b8ad525d50160b5ea4088d0052
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-04-11 01:09:27 +0600

    performance commit

--- bar-chart-with-label.py
+++ bar-chart-with-label.py
@@ -11,7 +11,7 @@ ax.set(ylabel='speed in MPH', title='Running speeds', ylim=(0, 80))
 ax.bar_label(bar_container, fmt=lambda x: f'{x * 1.61:.1f} km/h')
 
 plt.show()
-"""
+
 
 #bar chart with labels
 
@@ -36,3 +36,34 @@ for sex,sex_count in sex_counts.items():
 ax.set_title('Number of penguins by sex')
 ax.legend()
 plt.show()
+
+
+#Horizontal bars "performance"
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# Example data
+people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+fig, ax = plt.subplots()
+
+hbars = ax.barh(y_pos, performance, xerr=error, align='center')
+ax.set_yticks(y_pos, labels=people)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Performance')
+ax.set_title('How fast do you want to go today?')
+
+# Label with specially formatted floats
+ax.bar_label(hbars, fmt='%.2f')
+ax.set_xlim(right=15)  # adjust xlim to fit labels
+
+plt.show()
+"""
+#some advanced with bars
