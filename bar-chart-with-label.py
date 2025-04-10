4981b2dbda09f90fce957fefaf35b50d07571219
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-04-11 01:12:09 +0600

    advanced commit

--- bar-chart-with-label.py
+++ bar-chart-with-label.py
@@ -67,3 +67,28 @@ ax.set_xlim(right=15)  # adjust xlim to fit labels
 plt.show()
 """
 #some advanced with bars
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# Example data
+people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+fig, ax = plt.subplots()
+
+hbars = ax.barh(y_pos, performance, xerr=error, align='center')
+ax.set_yticks(y_pos, labels=people)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Performance')
+ax.set_title('How fast do you want to go today?')
+
+# Label with given captions, custom padding and annotate options
+ax.bar_label(hbars, labels=[f'±{e:.2f}' for e in error],
+             padding=8, color='b', fontsize=14)
+ax.set_xlim(right=16)
+
+plt.show()
