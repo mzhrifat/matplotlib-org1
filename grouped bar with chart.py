dab24e5fd137bf267cbf9907040d9973c8e69ca5
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-04-15 00:06:09 +0600

    again commit

--- grouped bar with chart.py
+++ grouped bar with chart.py
@@ -1,33 +1,33 @@
 
 # data from https://allisonhorst.github.io/palmerpenguins/
 
-import matplotlib.pyplot as plt
+import  matplotlib.pyplot as plt
 import numpy as np
 
-species = ("Adelie", "Chinstrap", "Gentoo")
-penguin_means = {
-    'Bill Depth': (18.35, 18.43, 14.98),
-    'Bill Length': (38.79, 48.83, 47.50),
-    'Flipper Length': (189.95, 195.82, 217.19),
-}
+species =("Adelie","Chinstrap","Gentoo")
+penguin_means={
+    "Bill Depth":(18.35,18.43,14.98),
+    "Bill Length":(38.79,48.83,47.50),
+    "Flipper Length":(189.95,195.82,219.19),
 
-x = np.arange(len(species))  # the label locations
-width = 0.25  # the width of the bars
+}
+x=np.arange(len(species))#the label locations
+width=0.25
 multiplier = 0
 
-fig, ax = plt.subplots(layout='constrained')
+fig,ax=plt.subplots(layout='constrained')
 
-for attribute, measurement in penguin_means.items():
-    offset = width * multiplier
-    rects = ax.bar(x + offset, measurement, width, label=attribute)
-    ax.bar_label(rects, padding=3)
+for attribute,measurement in penguin_means.items():
+    offset=width * multiplier
+    rects=ax.bar(x+offset,measurement,width,label=attribute)
+    ax.bar_label(rects,padding=3)
     multiplier += 1
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Length (mm)')
-ax.set_title('Penguin attributes by species')
-ax.set_xticks(x + width, species)
-ax.legend(loc='upper left', ncols=3)
-ax.set_ylim(0, 250)
+#Add some text for labels,title and custom x-axis tick labels,etc.
 
+ax.set_ylabel('length (mm)')
+ax.set_title('Penguin attributes by species')
+ax.set_xticks(x+width,species)
+ax.legend(loc='upper left',ncols=3)
+ax.set_ylim(0,259)
 plt.show()
