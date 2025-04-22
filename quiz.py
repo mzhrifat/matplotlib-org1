532c5dceaec23b9f1b9e84d6221eb00513530a07
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-04-23 01:56:46 +0600

    quiz commit

---
+++ quiz.py
@@ -0,0 +1,14 @@
+
+
+print("👋Well come this Quiz")
+print("What is the capital of Banglaesh\n")
+
+answer=input("Dhaka").lower()
+
+if answer =="Dhaka":
+    print("Correct!:You answer Right")
+
+else:
+    print("Wrong!:Write correct answer")
+
+
