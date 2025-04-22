c65cbfe77d7ba1bc42023d0b3c467057e8d5082b
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-04-23 01:15:21 +0600

    Categorial commit

--- Plotting categorical variables.py
+++ Plotting categorical variables.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
+from IPython.core.pylabtools import figsize
 
-cat = ["bored", "happy", "bored", "bored", "happy", "bored"]
-dog = ["happy", "happy", "happy", "happy", "bored", "bored"]
-activity = ["combing", "drinking", "feeding", "napping", "playing", "washing"]
+data={'apple':10,'orange':15,'lemon':5,'lime':20}
+names=list(data.keys())
+values=list(data.values())
 
-fig, ax = plt.subplots()
-ax.plot(activity, dog, label="dog")
-ax.plot(activity, cat, label="cat")
-ax.legend()
-
+fig,axs=plt.subplots(1,3,figsize=(9,3),sharey=True)
+axs[0].bar(names,values)
+axs[1].scatter(names,values)
+axs[2].plot(names,values)
+fig.suptitle('Categorial Plotting')
 plt.show()
