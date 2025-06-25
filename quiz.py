d0e0e5bcf9689a8710a0a5741a59054ad5fff0dc
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-06-26 01:02:46 +0600

    connecting all nation

--- quiz.py
+++ quiz.py
@@ -14,19 +14,26 @@ print (S[k-1])
 """
 """connecting a nation"""
 
-def find (parent,x);
-    if parent[x]!=x:
-        parent[x]=find(parent,parent[x])
-        return parent[x]
-def union(parent,x,y):
-    x_root=find(parent,x)
-    y_root = find(parent,y)
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
 
-if x_root!=y_root;
-    parent[x_root]=y_root
-n,m=map(int,input().split())
-parent=[i for i in range(n+1)]
+def union(parent, x, y):
+    x_root = find(parent, x)
+    y_root = find(parent, y)
+    if x_root != y_root:
+        parent[x_root] = y_root
 
-for _in range(m):
-    u,v = map(inr,input().split())
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
 
+for _ in range(m):
+    u, v = map(int, input().split())
+    union(parent, u, v)
+
+components = set()
+for i in range(1, n + 1):
+    components.add(find(parent, i))
+
+print(len(components) - 1)
