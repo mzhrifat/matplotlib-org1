a1afb69351d5136ca12357864fae02add3b0bec2
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-06-25 23:56:47 +0600

    all quiz commit

--- quiz.py
+++ quiz.py
@@ -1,11 +1,12 @@
 t=int(input())
-for _in range(t):
+for _ in range(t):
 n,c,q=map(int,input().split())
 s=input
-for _in range(c):
+for _ in range(c):
 l,r=map(int,input().split())
 s+ = s[l-1 : r]
 
-for _in range(q):
+for _ in range(q):
 k=int(input())
-print (s[k-1])
+
+print (S[k-1])
